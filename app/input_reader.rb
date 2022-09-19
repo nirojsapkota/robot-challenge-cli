dc069cc3c2776c70b883c5e0e0ac5b3c94ee554d
Niroj Sapkota <niroj@activebilling.com.au>
2022-09-19 10:05:12 +1000

    added validatin

--- app/input_reader.rb
+++ app/input_reader.rb
@@ -2,6 +2,7 @@ require 'byebug'
 # require 'commands/command'
 class InputReader
   VALID_COMMANDS = %w[left right move report].freeze
+  PLACE_CMD_REGEX = /^\d+,+(\s)*+\d+,+(\s)*+[a-zA-Z]+(\s)*+$/.freeze
 
   def initialize(robot, table)
     @robot = robot
@@ -24,11 +25,10 @@ class InputReader
     return if command_str.empty?
 
     command, args = command_str.strip.downcase.split(/\s+/, 2)
-    args = args.gsub(/\s/, '').split(',') if args
     case command
     when 'place'
-      raise InvalidCommandError, 'PLACE X,Y,F format not supplied' unless args.length == 3
-
+      validate_place_command(args)
+      args = args.gsub(/\s/, '').split(',')
       Commands::Place.new(@robot, @table, Position.new(*cast_to_int(args)))
     when *VALID_COMMANDS
       Object.const_get("Commands::#{command.capitalize}").new(@robot, @table)
@@ -41,5 +41,9 @@ class InputReader
     str_ar.map do |str|
       /\A\d+\Z/.match(str) ? str.to_i : str
     end
+  end
+
+  def validate_place_command(args)
+    raise InvalidCommandError, 'PLACE X,Y,F format not supplied' unless args =~ PLACE_CMD_REGEX
   end
 end
